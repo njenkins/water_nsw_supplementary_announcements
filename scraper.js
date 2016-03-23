249c311931c30836c8a036ed0d3f412a85e64732
njenkins <web@nathanjenkins.com.au>
2016-03-24 07:14:14 +1100

    Clean out old records and parse month/date from title

--- scraper.js
+++ scraper.js
@@ -7,22 +7,27 @@ function initDatabase(callback) {
 	// Set up sqlite database.
 	var db = new sqlite3.Database("data.sqlite");
 	db.serialize(function() {
-		db.run("CREATE TABLE IF NOT EXISTS data (title TEXT, url TEXT, year INTEGER)");
+		resetTable(db);
+		db.run("CREATE TABLE IF NOT EXISTS data (title TEXT, url TEXT, year INTEGER, daymonth STRING)");
 		callback(db);
 	});
 }
 
+function resetTable(db){
+	db.run("DROP TABLE IF EXISTS data");
+}
 function updateRow(db, values) {
 	// Insert some data.
-	var statement = db.prepare("INSERT INTO data(title, url, year) VALUES (?, ?, ?)");
+	var statement = db.prepare("INSERT INTO data(title, url, year, daymonth) VALUES (?, ?, ?, ?)");
 	statement.run(values);
 	statement.finalize();
 }
 
 function readRows(db) {
 	// Read some data.
-	db.each("SELECT rowid AS id, title, url, year FROM data", function(err, row) {
-		console.log(row.id + ": " + row.title + ': ' + row.url + ': ' + row.year);
+	db.each("SELECT rowid AS id, title, url, year, daymonth FROM data", function(err, row) {
+		console.log(row.daymonth + ' ' + row.year);
+		//console.log(row.id + ": " + row.title + ': ' + row.url + ': ' + row.daymonth +' ' + row.year);
 	});
 }
 
@@ -46,9 +51,20 @@ function run(db) {
 		var elements = $(".related-box ul .heading a").each(function () {
 			var $link = $(this);
 			var title = $link.text().trim();
+			//If title has a hyphen
+			if(title.indexOf('-') > -1){
+				var titleSplit = title.split('-');
+				var dayMonth;
+				title = titleSplit[0];
+				if(titleSplit[1]){
+					dayMonth = titleSplit[1].trim();
+				}
+			}
+
 			var url = $link.attr('href');
 			var year = parseInt($link.closest('.related-box').find('h2').text());
-			var values = [title, url, year];
+			var daymonth = 'test';
+			var values = [title, url, year, dayMonth];
 			updateRow(db, values);
 
 		});
